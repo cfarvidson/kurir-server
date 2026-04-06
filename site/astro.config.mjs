420cca51aa6810ded43bfb22de6d5b37fd77de87
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2026-04-06 14:42:56 +0200

    feat(site): switch base to root and add kurir.io CNAME for custom domain

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://cfarvidson.github.io",
-  base: process.env.LOCAL_DEV ? "/" : "/kurir-server/",
+  site: "https://kurir.io",
+  base: "/",
   integrations: [tailwind()],
 });
