f73611d557b4b5c6aae81401ca872548b16261cf
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2026-08-26 11:26:14 +0200

    site: calendar feature card, Mac app now on the Mac App Store

--- site/src/lib/store.ts
+++ site/src/lib/store.ts
@@ -1,2 +1,4 @@
 export const APP_STORE_IOS =
   "https://apps.apple.com/se/app/kurir-email/id6795541775?l=en-GB";
+export const APP_STORE_MAC =
+  "https://apps.apple.com/se/app/kurir-email/id6795541775?l=en-GB&mt=12";
